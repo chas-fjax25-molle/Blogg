95db07c2c3c382fdc5f124bb6a41315c508e522b
Oliver Öhrström <xzereha@live.se>
2026-01-20 08:29:53 +0100

    Added PWA

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
+    plugins: [
+        VitePWA({
+            registerType: "autoUpdate",
+            devOptions: { enabled: true }, // valfritt: gör att det funkar i dev
+            manifest: {
+                name: "Blog",
+                short_name: "Blog",
+                start_url: "/",
+                display: "standalone",
+                theme_color: "#ffffff",
+                background_color: "#ffffff",
+                icons: [
+                    { src: "pwa-192x192.png", sizes: "192x192", type: "image/png" },
+                    { src: "pwa-512x512.png", sizes: "512x512", type: "image/png" }
+                ]
+            }
+        })
+    ]
+});
