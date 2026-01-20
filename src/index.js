b872d2f0b7eb41ee741cb5545df46cfafd89fecc
Vera Witting <verawitting@gmail.com>
2026-01-20 10:58:45 +0100

    fixed some stuff

--- src/index.js
+++ src/index.js
@@ -8,52 +8,40 @@ renderPostPreviews();
  * Renders previews of all blog posts on the main page.
 */
 function renderPostPreviews() {
+    const postContainer = document.querySelector(".blog-posts");
+    
     if (!document.querySelector(".blog-posts")) {
         console.error("Blog posts container not found.");
         return;
     }
 
     blogPosts.forEach(post => {
-        renderPostPreview(post);
-    });
-}
-
-/**
- * Renders comment count of all blog comments on the main page.
-*/
-function renderCommentCount() {
-    if (!document.querySelector(".blog-posts")) {
-        console.error("Blog posts container not found.");
-        return;
-    }
-
-    blogPosts.forEach(post => {
-        renderPostPreview(post);
+        renderPostPreview(post, postContainer);
     });
 }
 
 /**
  * Renders a preview of a single blog post.
  * @param {BlogPost} post
+ * @param {HTMLElement} postContainer
  * TODO: add commentCount to post data
 */
-function renderPostPreview(post) {
+function renderPostPreview(post, postContainer) {
     const postComments = comments.filter(
       comment => comment.postId === post.id
     );
     const commentCount = postComments.length;
-    const postContainer = document.querySelector(".blog-posts");
     const postElement = document.createElement("article");
     postElement.className = "post-preview";
     postElement.id = `post-${post.id}`;
     postElement.innerHTML = `
         <h3 class="post-title">${post.title}</h3>
         <p class="post-excerpt">${post.excerpt}</p>
-        <img class="post-image-preview" src="${post.image}" alt="Blog Post Image" width="100" height="100"/>
-            <div class="post-preview-meta">    
-        <span class="comment-count" aria-label="Number of comments">
-            💬 ${commentCount ?? 0} comments
-        </span>
+        <img class="post-image-preview" src="${post.image}" alt="Preview image for blog post: ${post.title}" width="100" height="100"/>
+        <div class="post-preview-meta">    
+            <span class="comment-count" aria-label="Number of comments">
+                💬 ${commentCount ?? 0} comments
+            </span>
         </div>
         <button class="read-more-button" data-post-id="${post.id}" aria-label="Read More">Read More</button>
     `;
