bb4072e800e25646035ec7d065f45fb6abfb6a32
Vera Witting <verawitting@gmail.com>
2026-01-20 10:29:09 +0100

    added type definition to the comment data file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { blogPosts } from "./posts.js";
+import { comments } from "./comments.js";
 
 renderPostPreviews();
 
@@ -17,11 +18,29 @@ function renderPostPreviews() {
 }
 
 /**
+ * Renders comment count of all blog comments on the main page.
+*/
+function renderCommentCount() {
+    if (!document.querySelector(".blog-posts")) {
+        console.error("Blog posts container not found.");
+        return;
+    }
+
+    blogPosts.forEach(post => {
+        renderPostPreview(post);
+    });
+}
+
+/**
  * Renders a preview of a single blog post.
  * @param {BlogPost} post
  * TODO: add commentCount to post data
 */
 function renderPostPreview(post) {
+    const postComments = comments.filter(
+      comment => comment.postId === post.id
+    );
+    const commentCount = postComments.length;
     const postContainer = document.querySelector(".blog-posts");
     const postElement = document.createElement("article");
     postElement.className = "post-preview";
@@ -32,7 +51,7 @@ function renderPostPreview(post) {
         <img class="post-image-preview" src="${post.image}" alt="Blog Post Image" width="100" height="100"/>
             <div class="post-preview-meta">    
         <span class="comment-count" aria-label="Number of comments">
-            💬 ${post.commentCount ?? 0} comments
+            💬 ${commentCount ?? 0} comments
         </span>
         </div>
         <button class="read-more-button" data-post-id="${post.id}" aria-label="Read More">Read More</button>
