087e51af9ef6d8e749ca3f339507b2fd652f41cd
Vera Witting <verawitting@gmail.com>
2026-01-20 10:03:05 +0100

    added comments without the server part

--- src/index.js
+++ src/index.js
@@ -18,7 +18,8 @@ function renderPostPreviews() {
 
 /**
  * Renders a preview of a single blog post.
- * @param {BlogPost} post 
+ * @param {BlogPost} post
+ * TODO: add commentCount to post data
 */
 function renderPostPreview(post) {
     const postContainer = document.querySelector(".blog-posts");
@@ -29,6 +30,11 @@ function renderPostPreview(post) {
         <h3 class="post-title">${post.title}</h3>
         <p class="post-excerpt">${post.excerpt}</p>
         <img class="post-image-preview" src="${post.image}" alt="Blog Post Image" width="100" height="100"/>
+            <div class="post-preview-meta">    
+        <span class="comment-count" aria-label="Number of comments">
+            💬 ${post.commentCount ?? 0} comments
+        </span>
+        </div>
         <button class="read-more-button" data-post-id="${post.id}" aria-label="Read More">Read More</button>
     `;
 
