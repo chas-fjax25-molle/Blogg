3e2a05f14ca4a764ef974e920b369a3806b33f1f
Vera Witting <verawitting@gmail.com>
2026-01-20 10:13:10 +0100

    added comments.js file

---
+++ src/comments.js
@@ -0,0 +1,34 @@
+/**
+ * Comment data structure
+ * @type {Comment[]}
+ */
+export const comments = [
+    {
+        id: 1,
+        postId: 1,
+        author: "Vera",
+        text: "This explanation of closures finally made it click for me!",
+        date: "2024-05-12"
+    },
+    {
+        id: 2,
+        postId: 1,
+        author: "Pontus",
+        text: "Great examples, especially the part about private variables.",
+        date: "2024-05-13"
+    },
+    {
+        id: 3,
+        postId: 2,
+        author: "Millaray",
+        text: "Hooks felt scary before, this made them feel approachable.",
+        date: "2024-06-16"
+    },
+    {
+        id: 4,
+        postId: 2,
+        author: "Mario",
+        text: "Would love a follow-up post about custom hooks!",
+        date: "2024-06-17"
+    }
+];
